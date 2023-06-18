8e71b5d9511be49e71b62009a4d0631689e58292
Shengfeng233 <xu_shengfeng1220@163.com>
2023-06-18 19:04:47 +0800

    first commit

---
+++ STPINN-RANS/main.py
@@ -0,0 +1,127 @@
+"""
+2-dimensional unsteady RANS equation
+Spatio-temporal parallel----rectangle domain decomposition
+overlapping domain decomposition
+simultaneous training
+batch is split in iterations
+2维非定常-RANS方程
+时空并行——矩形子网络划分
+子网络有重叠域(Overlapping domain)
+采用同时训练的架构，划分batch
+x y t u v p
+"""
+import numpy as np
+from mpi4py import MPI
+from sub_functions import *
+
+
+comm = MPI.COMM_WORLD
+size = comm.Get_size()
+rank = comm.Get_rank()
+name = MPI.Get_processor_name()
+start = MPI.Wtime()
+dir_name_write = './sub_data/sub_write'
+filename_model = './sub_data/sub_write/{:02d}NS_model_train.pt'
+filename_save_model = filename_model.format(rank)
+filename_data = './sub_data/sub_read/stack_unsteady_{:02d}.mat'  # 训练数据 training data
+filename_loss = './sub_data/sub_write/{:02d}loss.csv'
+filename_loss = filename_loss.format(rank)
+filename_loss_overall = './loss.csv'
+device = torch.device("cpu")  # 使用CPU并行 parallel using CPU
+torch.manual_seed(3407)
+np.random.seed(3407)
+
+# important parameters
+# 重要参数
+time_division = 10
+extension_ratio = 0.05
+low_bound_all = np.array([1.0000, -2.0000, 0.0]).reshape(1, -1)  # x,y,t
+up_bound_all = np.array([4.98998, 2.0000, 42.93729]).reshape(1, -1)  # x,y,t
+dimension = 2 + 1
+Re = 3900  # 雷诺数 Reynolds number
+layer_mat_1 = [3, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 4]  # 网络结构 neural network
+layer_mat_2 = [3, 80, 80, 80, 80, 80, 4]   # 网络结构 neural network
+layer_mat_3 = [3, 50, 50, 50, 50, 50, 50, 50, 50, 50, 50, 4]   # 网络结构 neural network
+layer_mat = layer_mat_3
+learning_rate = 0.001  # 学习率 learning rate
+T_0 = 100  # 学习率控制参数 parameter used to control learning rate
+T_mult = 2
+batch_size = 128
+inner_epochs = 1  # 训练周期数——小周期数 epoch-inner epoch
+EPOCH_of_annealing = 7
+epoch_list = generate_epoch_list(T_0, T_mult, EPOCH_of_annealing)  # 回火周期数组 annealing epochs
+outer_epochs = epoch_list[0, -1]  # 训练周期数 epoch-outer epoch
+weight_of_data = 1
+weight_of_eqa = 1
+weight_pseudo = 1
+decay_rate = 0.9  # 用以控制最大学习率 to control learning rate decaying
+N_eqa = 8000
+pseudo_points_surface_set = np.array([25, 25, 10]).reshape(1, -1).astype(int)
+debug_key = 0
+local_debug_key = 0
+
+if local_debug_key == 1:
+    size = 40  # debug only
+    rank = 4  # debug only
+
+# pretraining-loading data
+# loading data points
+# loading collocation points
+# loading pseudo-data points(interface points)
+# only load once
+# 训练前加载工作(仅加载一次)
+# 加载数据点
+# 加载方程点
+# 加载伪数据点(边界)坐标
+True_dataset, Eqa_points_batches, sub_net_info, iter_num = pre_train_loading(filename_data, rank, size, time_division,
+                                                                             dimension, N_eqa, extension_ratio,
+                                                                             low_bound_all, up_bound_all, batch_size)
+
+up_coord, down_coord, left_coord, right_coord, front_coord, back_coord = identify_sub_boundaries(sub_net_info,
+                                                                                                 pseudo_points_surface_set)
+pinn_net = PINN_Net(layer_mat, sub_net_info.sub_lb, sub_net_info.sub_ub)
+pinn_net = pinn_net.to(device)  # 每一个进程创建一个网络 allocate a separate network for each process
+
+# 优化器和学习率衰减设置- optimizer and learning rate schedule
+optimizer_all = torch.optim.Adam(pinn_net.parameters(), lr=learning_rate)
+scheduler_all = ChainedScheduler(optimizer_all, T_0=T_0, T_mul=T_mult, eta_min=0, gamma=decay_rate, max_lr=learning_rate,
+                                 warmup_steps=0)
+
+# 训练主循环 main loop
+for EPOCH in range(outer_epochs):
+    # 对伪数据点(对边界)的预测 predict on the interface points
+    up_set, down_set, left_set, right_set, front_set, back_set = sub_predict_boundaries(pinn_net, up_coord, down_coord,
+                                                                                        left_coord, right_coord,
+                                                                                        front_coord, back_coord, Re)
+
+    # 子网络信息交互-(x,y,u,v,p) communicate among sub neural networks
+    boundary_set = communicate(rank, size, time_division, comm, sub_net_info, up_set, down_set, left_set, right_set,
+                               front_set, back_set)
+
+    # 制造伪数据点 generate pseudo-data points
+    Pseudo_dataset_batches = make_pseudo_data(boundary_set, pinn_net, iter_num)
+
+    # 对数据点，伪数据点，方程点进行同时训练 train
+    loss_sum, loss_data, loss_eqa, loss_pseudo = train(inner_epochs, pinn_net, optimizer_all, scheduler_all,
+                                                       iter_num, True_dataset, Eqa_points_batches,
+                                                       Pseudo_dataset_batches, Re, weight_of_data, weight_of_eqa,
+                                                       weight_pseudo, EPOCH, debug_key)
+
+    # loss记录 record loss
+    loss_set = record_loss_local(loss_sum, loss_data, loss_eqa, loss_pseudo, filename_loss)  # 记录子网络loss
+    loss_sum_all = record_loss_overall(comm, rank, loss_set, filename_loss_overall)  # 记录总loss至根节点,并分发至各个子节点
+    comm.Barrier()
+
+    # 在每一个回火周期保存各个子网络模型 save model at the end of each annealing epoch
+    if np.isin(EPOCH + 1, epoch_list).item():
+        annealing_num = np.where(EPOCH + 1 == epoch_list)[1].item() + 1
+        dir_name = dir_name_write + '/annealing' + str(annealing_num)
+        os.makedirs(dir_name, exist_ok=True)
+        torch.save(pinn_net.state_dict(), dir_name + '/{:02d}NS_model_train.pt'.format(rank))
+
+# 每个子网络保存模型 save the final model
+torch.save(pinn_net.state_dict(), filename_save_model)
+end = MPI.Wtime()
+if rank == 0:  # 根节点输出 print on the root process
+    print("all networks finished")
+    print("Time used: %d s" % (end - start))
---
+++ STPINN-RANS/pinn_model.py
@@ -0,0 +1,171 @@
+"""
+PINN+incompressible RANS equation
+2-dimensional unsteady
+PINN model +LOSS function
+PINN融合不可压缩RANS方程
+二维非定常流动
+PINN模型 + LOSS函数
+"""
+import os
+
+import numpy as np
+import torch
+import torch.nn as nn
+from pyDOE import lhs
+
+
+# define PINN net
+# 定义PINN网络模块，包括数据读取函数，参数初始化
+
+# GPU or CPU
+# 训练设备为GPU还是CPU
+device = torch.device("cpu")
+
+
+# Define network structure, specified by a list of layers indicating the number of layers and neurons
+# 定义网络结构,由layer列表指定网络层数和神经元数
+class PINN_Net(nn.Module):
+    def __init__(self, layer_mat, low_bound, up_bound):
+        super(PINN_Net, self).__init__()
+        self.lowbound = torch.from_numpy(low_bound.astype(np.float32))
+        self.upbound = torch.from_numpy(up_bound.astype(np.float32))
+        self.layer_num = len(layer_mat) - 1
+        self.base = nn.Sequential()
+        for i in range(0, self.layer_num - 1):
+            self.base.add_module(str(i) + "linear", nn.Linear(layer_mat[i], layer_mat[i + 1]))
+            self.base.add_module(str(i) + "Act", nn.Tanh())
+        self.base.add_module(str(self.layer_num - 1) + "linear",
+                             nn.Linear(layer_mat[self.layer_num - 1], layer_mat[self.layer_num]))
+        self.Initial_param()
+
+    # 0-1 norm of input variable
+    # 对输入变量进行0-1归一化
+    def zero_one_norm(self, X):
+        X_norm = (X-self.lowbound)/(self.upbound-self.lowbound)
+        return X_norm
+
+    def forward(self, x, y, t):
+        X = torch.cat([x, y, t], 1).requires_grad_(True)
+        X_norm = self.zero_one_norm(X)
+        predict = self.base(X_norm)
+        return predict
+
+    # initialize
+    # 对参数进行初始化
+    def Initial_param(self):
+        for name, param in self.base.named_parameters():
+            if name.endswith('linear.weight'):
+                nn.init.xavier_normal_(param)
+            elif name.endswith('linear.bias'):
+                nn.init.zeros_(param)
+
+    # derive loss for data
+    # 类内方法：求数据点的loss
+    def data_mse(self, x, y, t, u, v, p):
+        predict_out = self.forward(x, y, t)
+        u_predict = predict_out[:, 0].reshape(-1, 1)
+        v_predict = predict_out[:, 1].reshape(-1, 1)
+        p_predict = predict_out[:, 2].reshape(-1, 1)
+        mse = torch.nn.MSELoss()
+        mse_predict = mse(u_predict, u) + mse(v_predict, v) + mse(p_predict, p)
+        return mse_predict
+
+    # derive loss for data without pressure
+    # 类内方法：求数据点的loss(不含压力数据)
+    def data_mse_without_p(self, x, y, t, u, v):
+        predict_out = self.forward(x, y, t)
+        u_predict = predict_out[:, 0].reshape(-1, 1)
+        v_predict = predict_out[:, 1].reshape(-1, 1)
+        mse = torch.nn.MSELoss()
+        mse_predict = mse(u_predict, u) + mse(v_predict, v)
+        return mse_predict
+
+    # predict
+    # 类内方法：预测
+    def predict(self, x, y, t):
+        predict_out = self.forward(x, y, t)
+        u_predict = predict_out[:, 0].reshape(-1, 1)
+        v_predict = predict_out[:, 1].reshape(-1, 1)
+        p_predict = predict_out[:, 2].reshape(-1, 1)
+        return u_predict, v_predict, p_predict
+
+    # derive loss for equation
+    def equation_mse_dimensionless(self, x, y, t, Re):
+        predict_out = self.forward(x, y, t)
+        # 获得预测的输出u,v,p
+        u = predict_out[:, 0].reshape(-1, 1)
+        v = predict_out[:, 1].reshape(-1, 1)
+        p = predict_out[:, 2].reshape(-1, 1)
+        niu_t = predict_out[:, 3].reshape(-1, 1)
+        # 通过自动微分计算各个偏导数,其中.sum()将矢量转化为标量，并无实际意义
+        # first-order derivative
+        # 一阶导
+        u_x = torch.autograd.grad(u.sum(), x, create_graph=True)[0]
+        u_y = torch.autograd.grad(u.sum(), y, create_graph=True)[0]
+        u_t = torch.autograd.grad(u.sum(), t, create_graph=True)[0]
+        v_x = torch.autograd.grad(v.sum(), x, create_graph=True)[0]
+        v_y = torch.autograd.grad(v.sum(), y, create_graph=True)[0]
+        v_t = torch.autograd.grad(v.sum(), t, create_graph=True)[0]
+        p_x = torch.autograd.grad(p.sum(), x, create_graph=True)[0]
+        p_y = torch.autograd.grad(p.sum(), y, create_graph=True)[0]
+        # second-order derivative
+        # 二阶导
+        u_xx = torch.autograd.grad(u_x.sum(), x, create_graph=True)[0]
+        u_yy = torch.autograd.grad(u_y.sum(), y, create_graph=True)[0]
+        v_xx = torch.autograd.grad(v_x.sum(), x, create_graph=True)[0]
+        v_yy = torch.autograd.grad(v_y.sum(), y, create_graph=True)[0]
+        # residual
+        # 计算偏微分方程的残差
+        f_equation_mass = u_x + v_y
+        f_equation_x = u_t + (u * u_x + v * u_y) + p_x - 1.0 / Re * (1 + niu_t) * (u_xx + u_yy)
+        f_equation_y = v_t + (u * v_x + v * v_y) + p_y - 1.0 / Re * (1 + niu_t) * (v_xx + v_yy)
+        mse = torch.nn.MSELoss()
+        batch_t_zeros = torch.zeros_like(x, dtype=torch.float32, device=device)
+        mse_equation = mse(f_equation_x, batch_t_zeros) + mse(f_equation_y, batch_t_zeros) + \
+                       mse(f_equation_mass, batch_t_zeros)
+
+        return mse_equation
+
+
+# LHS method
+# 生成矩形域方程点-拉丁超立方采样
+def generate_eqp_lhs(low_bound, up_bound, dimension, points):
+    eqa_xyzt = low_bound + (up_bound - low_bound) * lhs(dimension, points)
+    per = np.random.permutation(eqa_xyzt.shape[0])
+    new_xyzt = eqa_xyzt[per, :]
+    Eqa_points = torch.from_numpy(new_xyzt).float()
+    return Eqa_points
+
+
+# generate a list of annealing cycles
+# 生成回火周期列表，以在不同回火周期结束时保存模型
+def generate_epoch_list(T_0, T_mult, EPOCH_of_annealing):
+    epoch_list = np.zeros((1, EPOCH_of_annealing))
+    T_i = T_0
+    epoch_list[0, 0] = T_i
+    for i in range(1, EPOCH_of_annealing):
+        T_i = T_mult * T_i
+        epoch_list[0, i] = T_i + epoch_list[0, i - 1]
+    return epoch_list.astype(int)
+
+
+def shuffle_data(x, y, t, u, v, p):
+    X_total = torch.cat([x, y, t, u, v, p], 1)
+    X_total_arr = X_total.data.numpy()
+    np.random.shuffle(X_total_arr)
+    X_total_random = torch.tensor(X_total_arr)
+    return X_total_random
+
+
+def simple_norm(x, y, t, u, v, p, feature_mat):
+    x = x / feature_mat[0, 0]
+    y = y / feature_mat[0, 1]
+    t = t / feature_mat[0, 2]
+    u = u / feature_mat[0, 3]
+    v = v / feature_mat[0, 4]
+    p = p / feature_mat[0, 5]
+    return x, y, t, u, v, p, feature_mat
+
+
+
+
